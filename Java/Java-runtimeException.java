946c71c7fe97cc213efc2266b7429faccb8b5689
wkimdev <wkimdev@gmail.com>
2018-12-27 09:24:10 +0900

    Update Java-runtimeException.java

--- Java/Java-runtimeException.java
+++ Java/Java-runtimeException.java
@@ -40,6 +40,35 @@ public class BoardMain {
 //		if(true) {
 //			throw new Exception();
 //		}
-		System.out.println("execute this output method");
+		//System.out.println("execute this output method");
+		
+				// classcastException
+		// case 1
+		Animal animal = new Dog();
+		// instanceof로 타입 변환이 가능한지 확인하기
+		Cat cat = (Cat) animal;
+//		if(animal instanceof Dog) {
+//			Dog dog = (Dog) animal;
+//			System.out.println("1111111");
+//		} else if(animal instanceof Cat) {
+//			Cat cat = (Cat) animal;
+//			System.out.println("222222");
+//		}
+			
+		// 참조변수(연산자) instanceof 타입(클래스명-피연산자)
+		// return type은 boolean
+		
+		// case 2
+		Remocon rc = new Audio();
+		if(rc instanceof Television) {
+			Television tv = (Television) rc ;
+			System.out.println("Television");
+		} else if (rc instanceof Audio) {
+			Audio audio = (Audio) rc;
+			System.out.println("Audio");
+		}
+
+		
+		
 	}
 }
